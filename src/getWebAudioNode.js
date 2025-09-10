6662c94df19b217d0d2e77ae02b9bc9d8fcf2316
miquelsempere <64729434+miquelsempere@users.noreply.github.com>
2025-09-10 14:30:08 +0200

    Updated getWebAudioNode.js

--- src/getWebAudioNode.js
+++ src/getWebAudioNode.js
@@ -3,7 +3,7 @@ import noop from './noop.js';
  * getWebAudioNode
  *
  * A wrapper to create an AudioNode and apply a filter for frame extraction
- * Copyright (c) Adrian Holovaty https://github.com/adrianholovaty
+ * Copyright (c) Adrian Holovary https://github.com/adrianholovaty
  *
  * @param context - AudioContext
  * @param filter - Object containing an 'extract()' method
@@ -18,42 +18,19 @@ const getWebAudioNode = function (
 ) {
   const node = context.createScriptProcessor(bufferSize, 2, 2);
   const samples = new Float32Array(bufferSize * 2);
-  let hasEnded = false;
 
   node.onaudioprocess = (event) => {
-    // If the track has already ended, fill with silence and return
-    if (hasEnded) {
-      let left = event.outputBuffer.getChannelData(0);
-      let right = event.outputBuffer.getChannelData(1);
-      left.fill(0);
-      right.fill(0);
-      return;
-    }
-
     let left = event.outputBuffer.getChannelData(0);
     let right = event.outputBuffer.getChannelData(1);
     let framesExtracted = filter.extract(samples, bufferSize);
     sourcePositionCallback(filter.sourcePosition);
-    
     if (framesExtracted === 0) {
-      if (!hasEnded) {
-        hasEnded = true;
-        // Disconnect the node to stop further processing
-        node.disconnect();
-        filter.onEnd();
-      }
+      filter.onEnd();
     }
-    
     let i = 0;
     for (; i < framesExtracted; i++) {
       left[i] = samples[i * 2];
       right[i] = samples[i * 2 + 1];
-    }
-    
-    // Fill remaining buffer with silence if we extracted fewer frames than buffer size
-    for (; i < bufferSize; i++) {
-      left[i] = 0;
-      right[i] = 0;
     }
   };
   return node;
