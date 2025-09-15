fa72facc43587534c2087d8543733faeb04c4b2b
miquelsempere <64729434+miquelsempere@users.noreply.github.com>
2025-09-15 13:35:39 +0200

    Updated package.json

--- src/SimpleFilter.js
+++ src/SimpleFilter.js
@@ -20,8 +20,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-import FilterSupport from './FilterSupport.js';
-import noop from './noop.js';
+import FilterSupport from './FilterSupport';
+import noop from './noop';
 
 export default class SimpleFilter extends FilterSupport {
   constructor(sourceSound, pipe, callback = noop) {
--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-import AbstractFifoSamplePipe from './AbstractFifoSamplePipe.js';
-import RateTransposer from './RateTransposer.js';
-import SimpleFilter from './SimpleFilter.js';
-import Stretch from './Stretch.js';
-import SoundTouch from './SoundTouch.js';
-import WebAudioBufferSource from './WebAudioBufferSource.js';
-import PitchShifter from './PitchShifter.js';
-import getWebAudioNode from './getWebAudioNode.js';
+import AbstractFifoSamplePipe from './AbstractFifoSamplePipe';
+import RateTransposer from './RateTransposer';
+import SimpleFilter from './SimpleFilter';
+import Stretch from './Stretch';
+import SoundTouch from './SoundTouch';
+import WebAudioBufferSource from './WebAudioBufferSource';
+import PitchShifter from './PitchShifter';
+import getWebAudioNode from './getWebAudioNode';
 
 export {
   AbstractFifoSamplePipe,
